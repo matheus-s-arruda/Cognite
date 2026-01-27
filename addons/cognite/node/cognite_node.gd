311606b01e5fa447a979ec4ae92b53090eeb4d41
matheus arruda <59703897+matheus-s-arruda@users.noreply.github.com>
2026-01-27 09:55:51 -0300

    runtime

--- addons/cognite/node/cognite_node.gd
+++ addons/cognite/node/cognite_node.gd
@@ -9,7 +9,7 @@ var perceptions: Dictionary
 var context: Dictionary
 var updating: bool
 
-var runtime_context: Array[CogniteRuntimeContext]
+var runtime_context: Dictionary
 var runtime_decision: Dictionary
 var decisions: Dictionary
 
@@ -22,7 +22,7 @@ func _enter_tree() -> void:
 		perceptions[per.name] = false if per.type == 0 else 0.0
 	
 	for context_id in cognite_assemble.contexts:
-		runtime_context.append(CogniteRuntimeContext.new(cognite_assemble.contexts[context_id], cognite_assemble, self))
+		runtime_context[context_id] = CogniteRuntimeContext.new(cognite_assemble.contexts[context_id], cognite_assemble, self)
 	
 	for decision_id in cognite_assemble.decisions:
 		decisions[decision_id] = 0
@@ -40,17 +40,18 @@ func _process(delta: float) -> void:
 	
 	var keys = decisions.keys()
 	keys.sort_custom(sort_decision_score)
+	#keys.reverse()
+	print(decisions[keys[0]])
+	#var decision_best_match := 0
+	#var decision_best_match_value := 0
+	#for i in decisions.size() -1:
+		#if decisions.keys()[i] > decisions.keys()[i + 1]:
+			#decision_best_match = decisions.keys()[i]
+			#decision_best_match_value
+		#else:
+			#decision_best_match = decisions.keys()[i + 1]
 	
-	var decision_best_match := 0
-	var decision_best_match_value := 0
-	for i in decisions.size() -1:
-		if decisions.keys()[i] > decisions.keys()[i + 1]:
-			decision_best_match = decisions.keys()[i]
-			decision_best_match_value
-		else:
-			decision_best_match = decisions.keys()[i + 1]
-	
-	print(cognite_assemble.decisions[decision_best_match].name, " : ")
+	#print(cognite_assemble.decisions[decision_best_match].name, " : ")
 
 
 func is_cognite_node():
